75e8d34e76373b6562e1f0a13ae15573ddefbfd7
Gabriel Santo <gvgesanto2@gmail.com>
2022-07-03 19:00:47 -0300

    feat: Add styles for the Homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,35 +1,39 @@
+import './homepage.styles.scss';
+
 export default function HomePage() {
   return (
-    <div className="home__content">
+    <>
       <h1 className="home__title">Welcome to our page!</h1>
-      <p className="home__text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quam
-        deserunt reiciendis libero, quia sint rem ipsa at aliquid veritatis
-        corrupti nam, praesentium ab accusamus dolores, esse laudantium ratione
-        perferendis! Itaque, earum odio velit harum ipsa amet repellat optio
-        possimus! Velit animi non tempore modi veniam, itaque odio voluptates
-        hic facilis provident mollitia at doloremque distinctio minus ad autem
-        sit perferendis temporibus nobis quod fugit! Officia optio laudantium
-        modi provident vel consequatur, tempore ducimus adipisci vero at.
-        Consequatur aliquam autem temporibus dolore incidunt porro! Harum
-        eligendi provident velit architecto doloribus veritatis exercitationem
-        adipisci porro. Debitis, et laudantium! Officia laboriosam fugit nihil
-        temporibus similique quas mollitia aperiam quisquam incidunt vero est
-        cumque eveniet sit ut qui, unde repudiandae ducimus magnam sint
-        necessitatibus eligendi amet. Odit recusandae commodi sunt illum
-        assumenda, nostrum non, veritatis laudantium consectetur magnam qui
-        numquam maiores facilis eveniet iste cumque dolorem ea culpa
-        voluptatibus quas adipisci earum. Porro tenetur et soluta qui labore
-        autem vero molestias amet?
-      </p>
-      <p className="home__text">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque
-        repellat quo ullam quaerat culpa vero vitae, sint voluptatibus saepe
-        incidunt! Est nobis consequatur illum voluptas sed at explicabo,
-        necessitatibus repellat blanditiis cum sapiente vero adipisci laborum
-        eius laboriosam alias sequi aspernatur nisi. Numquam repellendus nam
-        quos nesciunt illo, ratione sequi.
-      </p>
-    </div>
+      <div className="home__text-box">
+        <p className="home__text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quam
+          deserunt reiciendis libero, quia sint rem ipsa at aliquid veritatis
+          corrupti nam, praesentium ab accusamus dolores, esse laudantium
+          ratione perferendis! Itaque, earum odio velit harum ipsa amet repellat
+          optio possimus! Velit animi non tempore modi veniam, itaque odio
+          voluptates hic facilis provident mollitia at doloremque distinctio
+          minus ad autem sit perferendis temporibus nobis quod fugit! Officia
+          optio laudantium modi provident vel consequatur, tempore ducimus
+          adipisci vero at. Consequatur aliquam autem temporibus dolore incidunt
+          porro! Harum eligendi provident velit architecto doloribus veritatis
+          exercitationem adipisci porro. Debitis, et laudantium! Officia
+          laboriosam fugit nihil temporibus similique quas mollitia aperiam
+          quisquam incidunt vero est cumque eveniet sit ut qui, unde repudiandae
+          ducimus magnam sint necessitatibus eligendi amet. Odit recusandae
+          commodi sunt illum assumenda, nostrum non, veritatis laudantium
+          consectetur magnam qui numquam maiores facilis eveniet iste cumque
+          dolorem ea culpa voluptatibus quas adipisci earum. Porro tenetur et
+          soluta qui labore autem vero molestias amet?
+        </p>
+        <p className="home__text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque
+          repellat quo ullam quaerat culpa vero vitae, sint voluptatibus saepe
+          incidunt! Est nobis consequatur illum voluptas sed at explicabo,
+          necessitatibus repellat blanditiis cum sapiente vero adipisci laborum
+          eius laboriosam alias sequi aspernatur nisi. Numquam repellendus nam
+          quos nesciunt illo, ratione sequi.
+        </p>
+      </div>
+    </>
   );
 }
